f28f24a746f8cf4a3618c4b8a33df9e816860e5a
Hokyeong Choi <chhk7449@gmail.com>
2023-01-03 22:28:56 +0900

    initial commit: - 렌더링된 엘리먼트 업데이트 추가

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import ReactDOM from 'react-dom';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  //js 코드는 함수 안에 들어가면 된다.
+  const root = ReactDOM.createRoot(document.getElementById("root"));
+
+  function tick() {
+    const element = (
+      <div>
+        <h1>Hello, world!</h1>
+        <h2>It is {new Date().toLocaleTimeString()}.</h2>
+      </div>
+    );
+    root.render(element);
+  }
+
+  setInterval(tick, 1000);
+
+  //html 코드는 return에 들어가면 된다.
+  return <div id="root">root</div>;
 }
 
 export default App;
